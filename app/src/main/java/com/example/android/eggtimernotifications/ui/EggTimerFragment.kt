104df06b5be387890ced726fccba49b3ec95bf44
sardarkhan <sardarkhan299@gmail.com>
2020-01-06 04:21:57 +0500

    Add topic to subscribe.

--- app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
@@ -60,6 +60,8 @@ class EggTimerFragment : Fragment() {
             getString(R.string.breakfast_notification_channel_name)
         )
 
+        subscribeTopic()
+
         return binding.root
     }
 
@@ -90,6 +92,19 @@ class EggTimerFragment : Fragment() {
 
         // TODO: Step 1.6 END create a channel
 
+    }
+
+    private fun subscribeTopic() {
+        // [START subscribe_topics]
+        FirebaseMessaging.getInstance().subscribeToTopic(TOPIC)
+            .addOnCompleteListener { task ->
+                var msg = getString(R.string.message_subscribed)
+                if (!task.isSuccessful) {
+                    msg = getString(R.string.message_subscribe_failed)
+                }
+                Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+            }
+        // [END subscribe_topics]
     }
 
     companion object {
